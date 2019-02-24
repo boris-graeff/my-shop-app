4c23cc4f210b7ac45233952fa8c6e807a40ccd12
bof <boris.graeff@gmail.com>
2019-02-24 15:16:39 +0100

    List cart products in cart page

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="cart">
-
+    <ul>
+      <li v-for="product in products" :key="product.key"
+          class="product" >
+        <img :src="product.thumbnail.image_url" :alt="`${product.name} thumbnail`" />
+        <div class="title">{{ product.name }}</div>
+        <div class="description">
+          {{ product.tagline }}
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+  import { mapState } from 'vuex'
+
 export default {
-  name: 'cart'
+  name: 'cart',
+  computed: {
+    ...mapState('cart', {
+      products: state => state.products
+    })
+  }
 }
 </script>
