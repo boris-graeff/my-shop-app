f96fb89e983763f701c659d1b305cad089e6d45f
bof <boris.graeff@gmail.com>
2019-02-24 14:17:47 +0100

    Create products and cart empty pages

--- src/router.js
+++ src/router.js
@@ -1,23 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Products from './views/Products'
+import Cart from './views/Cart'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '/products',
+      name: 'products',
+      component: Products
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/cart',
+      name: 'cart',
+      component: Cart
+    },
+    {
+      path: '*',
+      redirect: {
+        name: 'products'
+      }
     }
   ]
 })
---
+++ src/views/Cart.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="cart">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cart'
+}
+</script>
