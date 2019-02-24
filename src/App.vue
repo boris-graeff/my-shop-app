8c8c4b54b8d4db96d87bad8ed43d86509f82c63f
bof <boris.graeff@gmail.com>
2019-02-24 14:28:20 +0100

    Rewrite app header and nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,41 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link :to="{name: 'products'}">Products</router-link> |
-      <router-link :to="{name: 'cart'}">Cart</router-link>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <router-link :to="{name: 'products'}">Products</router-link>
+          </li>
+          <li>
+            <router-link :to="{name: 'cart'}">Cart</router-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <div class="content">
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
+
+<style lang="scss">
+  @import '~@/style/reset.scss';
+</style>
+
+<style scoped lang="scss">
+  header {
+    ul {
+      display: flex;
+      list-style: none;
+      padding: 0;
+    }
+
+    li {
+      padding: 10px;
+
+      :not(:last-child):after {
+        content: ' | ';
+      }
+    }
+  }
+</style>
